1d9ad168b3572fe28a5cb3c7c0f2031945b9b322
Auto Deploy <deploy@antigravity>
2026-03-21 11:52:44 +0700

    Fix Vercel 404 router issue and add Sindangjaya 5 seeder data

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { runSindangjayaSeeder } from './utils/seedSindangjaya.js'
 import { runSindangjaya3Seeder } from './utils/seedSindangjaya3.js'
+import { runSindangjaya5Seeder } from './utils/seedSindangjaya5.js'
 import { runResetStock } from './utils/resetStock.js'
 
 // runSindangjayaSeeder()
 // runSindangjaya3Seeder()
 // runResetStock()
+runSindangjaya5Seeder()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
